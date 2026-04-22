c1a48ce439cb138b472085ed4aac76f34fd3c868
cyrusivan <cyrusivancruz2002@gmail.com>
2026-04-22 11:50:36 +0800

    refactor: update text input component usage example and improve item form structure

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,6 +1,6 @@
 {{--
     USAGE:
-        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
 
     PROPS:
         disabled (optional) — boolean to disable the input field
@@ -8,4 +8,5 @@
 
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled)
+    {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
