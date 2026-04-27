fff1d819837c939ef266d61130b22f5b231d0a71
cyrusivan <cyrusivancruz2002@gmail.com>
2026-04-28 07:41:58 +0800

    feat: add autocomplete=off attribute to search input for improved accessibility

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -21,6 +21,7 @@
                 </svg>
             </div>
             <input type="text" name="search" value="{{ request('search') }}" id="{{ $id }}"
+                autocomplete="off"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full sm:w-64 pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Search">
         </div>
