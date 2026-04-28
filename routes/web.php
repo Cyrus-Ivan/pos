8ce5d8890731d07bb50db614ede60d3b7d971313
cyrusivan <cyrusivancruz2002@gmail.com>
2026-04-28 15:53:44 +0800

    feat: add update and destroy routes for inventory management with owner-admin middleware

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('/employees', 'employees')->name('employees')->middleware('can:owner');
     Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory')->middleware('can:owner-admin');
     Route::post('/inventory', [InventoryController::class, 'create'])->name('inventory.create')->middleware('can:owner-admin');
+    Route::put('/inventory', [InventoryController::class, 'update'])->name('inventory.update')->middleware('can:owner-admin');
+    Route::delete('/inventory', [InventoryController::class, 'destroy'])->name('inventory.destroy')->middleware('can:owner-admin');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
