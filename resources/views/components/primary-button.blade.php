c823cde6c00c2cb2ece5a4a23a7e507e95728535
cicruz <cyrusivancruz2002@gmail.com>
2026-05-02 17:23:16 +0800

    fix: adjust text tracking in primary button component for improved readability

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -9,6 +9,6 @@
 --}}
 
 <button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-indigo-700 active:bg-indigo-800 dark:active:bg-indigo-600 active:scale-95 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-slate-950 transition ease-in-out duration-150']) }}>
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white tracking-wide hover:bg-indigo-700 active:bg-indigo-800 dark:active:bg-indigo-600 active:scale-95 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-slate-950 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
