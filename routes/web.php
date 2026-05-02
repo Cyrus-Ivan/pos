006b01e047f100e8388c13b2f5584007ba7f1e61
cicruz <cyrusivancruz2002@gmail.com>
2026-05-02 17:51:57 +0800

    fix: reorganize route definitions for better structure and readability

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PosController::class, 'index'])->middleware(['auth'])->name('pos');
 
 Route::middleware(['auth',])->group(function () {
+    // pos
+    Route::post('/pos/cart/add', [PosController::class, 'addToCart'])->name('pos.cart.add');
+    Route::get('/pos/checkout', [PosController::class, 'checkout'])->name('pos.checkout');
+
+    // sales
     Route::view('/sales', 'sales')->name('sales');
+
+    // employees
     Route::view('/employees', 'employees')->name('employees')->middleware('can:owner');
+
+    // inventory
     Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory')->middleware('can:owner-admin');
     Route::post('/inventory', [InventoryController::class, 'create'])->name('inventory.create')->middleware('can:owner-admin');
     Route::put('/inventory', [InventoryController::class, 'update'])->name('inventory.update')->middleware('can:owner-admin');
     Route::delete('/inventory', [InventoryController::class, 'destroy'])->name('inventory.destroy')->middleware('can:owner-admin');
+
+    // employees + users
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
